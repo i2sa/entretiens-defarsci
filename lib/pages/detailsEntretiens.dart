100be0c23d74d26edff8279d97768c11f4693bb9
i2sa <issasow1494@gmail.com>
2023-12-13 17:51:43 +0000

    project entrtiens defarsci v2

--- lib/pages/detailsEntretiens.dart
+++ lib/pages/detailsEntretiens.dart
@@ -1,18 +1,143 @@
+import 'package:entretiens_defarsci/pages/model.dart';
 import 'package:flutter/material.dart';
 
-class DetailsEntretiens extends StatefulWidget {
-  const DetailsEntretiens({super.key});
-
-  @override
-  State<DetailsEntretiens> createState() => _detailsEntretiensState();
-}
-
-class _detailsEntretiensState extends State<DetailsEntretiens> {
+class DetailsEntretiens extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final Map<String, dynamic> arguments =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+
+    int id = arguments["id"];
+
+    int age = arguments['age'];
+    return MaterialApp(
+      color: Colors.white,
+      title: "details candidat ",
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Text("details"),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 233, 101, 29),
+          leading: ElevatedButton(
+              style: ButtonStyle(),
+              onPressed: () {
+                () => Navigator.pop(
+                      context,
+                    );
+              },
+              child: const Icon(
+                size: 30,
+                Icons.arrow_back,
+              )),
+          title: const Text(
+            "details candidat",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: ListView.builder(
+          itemCount: 1,
+          itemBuilder: (context, index) {
+            return Card(
+                child: ListTile(
+              title: Column(
+                children: [
+                  Text(
+                    'prenom: ${listEntretiens[id].prenom}',
+                    style: TextStyle(color: Colors.black),
+                    textAlign: TextAlign.left,
+                  ),
+                  Text(
+                    ' nom: ${listEntretiens[id].nom}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    'telephone: ${listEntretiens[id].telephone}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    ' Addresse: ${listEntretiens[id].Adresse}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    'pourquoi_ce_choix: ${listEntretiens[id].pourquoi_ce_choix}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    ' participants: ${listEntretiens[id].participants}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    'presentation_du_candidat: ${listEntretiens[id].presentation_du_candidat}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    ' connaissez_vous_defarsci: ${listEntretiens[id].connaissez_vous_defarsci}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    'comment_voyez_vous_defarsci: ${listEntretiens[id].comment_voyez_vous_defarsci}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    ' qu_entendez_de_defarsci: ${listEntretiens[id].qu_entendez_de_defarsci}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    'atouts: ${listEntretiens[id].atouts}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    ' faiblesses: ${listEntretiens[id].faiblesses}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    'maladie_ou_allergie: ${listEntretiens[id].maladie_ou_allergie}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    ' objectifs_dans_2ans: ${listEntretiens[id].objectifs_dans_2ans}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    'mois_de_formation: ${listEntretiens[id].mois_de_formation}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    ' demarrage: ${listEntretiens[id].demarrage}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    'disponibilite_du_candidat: ${listEntretiens[id].disponibilite_du_candidat}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    ' demarrage: ${listEntretiens[id].modalite_paiement}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    'number_en_cas_d_urgence: ${listEntretiens[id].number_en_cas_d_urgence}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    ' informations_supplementaires: ${listEntretiens[id].informations_supplementaires}',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
+              subtitle: Row(
+                children: [
+                  Text(
+                    ' email: ${listEntretiens[id].email}',
+                    style: TextStyle(color: Color.fromARGB(255, 233, 101, 29)),
+                  ),
+                  Text(
+                    ' domaine: ${listEntretiens[id].domaine}',
+                    style: TextStyle(color: Color.fromARGB(255, 233, 101, 29)),
+                  )
+                ],
+              ),
+            ));
+          },
+        ),
       ),
     );
   }
--- lib/pages/liste_entretiens.dart
+++ lib/pages/liste_entretiens.dart
@@ -41,27 +41,60 @@ class MyList extends StatelessWidget {
       title: 'liste entretien',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('liste entretien'),
+          backgroundColor: Color.fromARGB(255, 132, 173, 219),
+          title: Text(
+            'liste entretien',
+            style: TextStyle(
+                color: Color.fromARGB(255, 247, 247, 248),
+                fontWeight: FontWeight.bold,
+                fontSize: 25),
+          ),
         ),
         body: ListView.builder(
           itemCount: listEntretiens.length,
           itemBuilder: (context, index) {
+            int id = index;
             return Card(
+                color: Colors.white,
+                margin: EdgeInsets.all(5),
                 child: ListTile(
-              onTap: () => Navigator.pushNamed(context, '/details-entretiens'),
-              title: Row(
-                children: [
-                  Text('prenom: ${listEntretiens[index].prenom}'),
-                  Text(' nom: ${listEntretiens[index].nom}'),
-                ],
-              ),
-              subtitle: Row(
-                children: [
-                  Text(' email: ${listEntretiens[index].email}'),
-                  Text(' domaine: ${listEntretiens[index].domaine}')
-                ],
-              ),
-            ));
+                  onTap: () => Navigator.pushNamed(
+                    context,
+                    '/details-entretiens',
+                    arguments: {'id': id, 'age': 25},
+                  ),
+                  title: Row(
+                    children: [
+                      Text(
+                        'prenom: ${listEntretiens[id].prenom}',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          ' nom: ${listEntretiens[id].nom}',
+                          textAlign: TextAlign.end,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
+                  subtitle: Row(
+                    children: [
+                      Text(
+                        ' email: ${listEntretiens[id].email}',
+                        style: TextStyle(),
+                      ),
+                      Expanded(
+                          flex: 4,
+                          child: Text(
+                            ' domaine: ${listEntretiens[id].domaine}',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 233, 101, 29)),
+                          ))
+                    ],
+                  ),
+                ));
           },
         ),
       ),
